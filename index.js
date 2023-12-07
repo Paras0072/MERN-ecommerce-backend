5433126299c4632a31ef7c539a7321b87d3a246b
Paras Thakur <parasthakur007241@gmail.com>
2023-12-07 16:57:08 +0530

    Backend Part 2 : User, Cart, Orders added

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const server =express();
 const mongoose = require('mongoose');
+const cors = require("cors");
 const { createProduct } = require('./controller/Product');
-const productsRouters =require('./routes/Products')
-const categoriesRouters = require("./routes/Category");
-const brandsRouters = require("./routes/Brands");
-const cors=require("cors");
+const productsRouter =require('./routes/Products')
+const categoriesRouter = require("./routes/Category");
+const brandsRouter = require("./routes/Brands");
+const userRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter =require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
+
 // middlewares
+
 server.use(cors({
 exposedHeaders : ['X-Total-Count']
 
 }));
 server.use(express.json()); // to parse req.body
-server.use('/products',productsRouters.router);
-server.use("/categories", categoriesRouters.router);
-server.use("/brands", brandsRouters.router);
+server.use('/products',productsRouter.router);
+server.use("/categories", categoriesRouter.router);
+server.use("/brands", brandsRouter.router);
+server.use("/users", userRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", cartRouter.router);
+server.use("/orders", ordersRouter.router);
+
 main().catch(err=>console.log(err));
 async function main(){
 
